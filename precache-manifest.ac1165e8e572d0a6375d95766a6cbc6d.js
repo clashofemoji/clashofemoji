2292d5b136fd6acac057a6511765652b6537473e
Konrad Abe <mail@allbitsequal.com>
2019-05-10 16:08:20 +0200

    initial push with game pre alpha demo

---
+++ precache-manifest.ac1165e8e572d0a6375d95766a6cbc6d.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6733ce3348ae1abb4d20719387bd009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c78535769732052d7903",
+    "url": "/static/css/main.14e0f476.chunk.css"
+  },
+  {
+    "revision": "308c2794602be13f3f88",
+    "url": "/static/js/2.a7da31bd.chunk.js"
+  },
+  {
+    "revision": "c78535769732052d7903",
+    "url": "/static/js/main.c55fde88.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a284227254f12141b9573eab7a2811e",
+    "url": "/static/media/fredericka-the-great-v7-latin-regular.1a284227.ttf"
+  },
+  {
+    "revision": "844c18aa4f65140d0b3c19fce844aef9",
+    "url": "/static/media/fredericka-the-great-v7-latin-regular.844c18aa.woff"
+  },
+  {
+    "revision": "9e9696621c93a110718367a3d6e60b1f",
+    "url": "/static/media/fredericka-the-great-v7-latin-regular.9e969662.woff2"
+  },
+  {
+    "revision": "314ed89e5a0c4a67756c56e0c73f2c01",
+    "url": "/static/media/lilita-one-v6-latin-regular.314ed89e.woff2"
+  },
+  {
+    "revision": "444d2290afc278cc20f55776d08da0d8",
+    "url": "/static/media/lilita-one-v6-latin-regular.444d2290.woff"
+  },
+  {
+    "revision": "e6abad8018745c5592319dad13c91fc2",
+    "url": "/static/media/lilita-one-v6-latin-regular.e6abad80.ttf"
+  },
+  {
+    "revision": "10ae604f4dee72b9cbfbf082c91177b7",
+    "url": "/static/media/rubik-v8-latin-300.10ae604f.ttf"
+  },
+  {
+    "revision": "239cf85916292b3585d17ba1f7801dbb",
+    "url": "/static/media/rubik-v8-latin-300.239cf859.woff2"
+  },
+  {
+    "revision": "73ce1e5f9e44d490f48490673038d5d3",
+    "url": "/static/media/rubik-v8-latin-300.73ce1e5f.woff"
+  },
+  {
+    "revision": "29b164482600fc3976b5b2c863d69b71",
+    "url": "/static/media/rubik-v8-latin-700.29b16448.woff"
+  },
+  {
+    "revision": "a7db29488272756e766b024e6aebe848",
+    "url": "/static/media/rubik-v8-latin-700.a7db2948.woff2"
+  },
+  {
+    "revision": "c7093bf01697d1161db2a469c3c96da2",
+    "url": "/static/media/rubik-v8-latin-700.c7093bf0.ttf"
+  },
+  {
+    "revision": "4ff19ede4cf2a2a07c638795a389f6df",
+    "url": "/static/media/rubik-v8-latin-italic.4ff19ede.woff2"
+  },
+  {
+    "revision": "99647f8e523f3edb0eee394e66375e52",
+    "url": "/static/media/rubik-v8-latin-italic.99647f8e.ttf"
+  },
+  {
+    "revision": "f778b3151e4b3ed5b831e9e8646ab207",
+    "url": "/static/media/rubik-v8-latin-italic.f778b315.woff"
+  },
+  {
+    "revision": "17bedde315941b70131ecf25e34e8f47",
+    "url": "/static/media/rubik-v8-latin-regular.17bedde3.ttf"
+  },
+  {
+    "revision": "63c5b6176f60881d53dd2dc10904a04c",
+    "url": "/static/media/rubik-v8-latin-regular.63c5b617.woff2"
+  },
+  {
+    "revision": "80a9477d95ed5ca6e64843c1024e9e7f",
+    "url": "/static/media/rubik-v8-latin-regular.80a9477d.woff"
+  },
+  {
+    "revision": "5fb8b6fc36c4131b485e76b5834c02f7",
+    "url": "/static/media/ubuntu-mono-v8-latin-700.5fb8b6fc.woff2"
+  },
+  {
+    "revision": "ad755437adc718d060c2ebe9e76b47f6",
+    "url": "/static/media/ubuntu-mono-v8-latin-700.ad755437.woff"
+  },
+  {
+    "revision": "be60fa7096166f8929aab9729e9b78b8",
+    "url": "/static/media/ubuntu-mono-v8-latin-700.be60fa70.ttf"
+  },
+  {
+    "revision": "469ee47867fa81c9eb1677cb6df36493",
+    "url": "/static/media/ubuntu-mono-v8-latin-regular.469ee478.woff2"
+  },
+  {
+    "revision": "84924f07cdf2bc11727cbdb92bdecb60",
+    "url": "/static/media/ubuntu-mono-v8-latin-regular.84924f07.woff"
+  },
+  {
+    "revision": "e8562176acc125c0be02880bf8ff814d",
+    "url": "/static/media/ubuntu-mono-v8-latin-regular.e8562176.ttf"
+  },
+  {
+    "revision": "4dc7de3347f48884a7fb27e0ba6b8f6b",
+    "url": "/static/media/vt323-v10-latin-regular.4dc7de33.woff2"
+  },
+  {
+    "revision": "6a98b6bbf5f79b15e459956553250fa9",
+    "url": "/static/media/vt323-v10-latin-regular.6a98b6bb.ttf"
+  },
+  {
+    "revision": "d037f8cb75b5764ae757659ae551cc62",
+    "url": "/static/media/vt323-v10-latin-regular.d037f8cb.woff"
+  }
+]);
